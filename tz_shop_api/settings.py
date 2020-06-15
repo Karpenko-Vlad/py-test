243aa242718b0ac311c2a68e240f4809928a2cad
Vladyslav <karpenkovladyslavi@gmail.com>
2020-06-15 08:39:37 +0300

    add staticfiles dirs

--- tz_shop_api/settings.py
+++ tz_shop_api/settings.py
@@ -127,6 +127,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 AUTH_USER_MODEL = 'accounts.User'
 
 # DJANGO REST FRAMEWORK SETTINGS
